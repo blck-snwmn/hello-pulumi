90a4981ebbd6eac86f21445817b2f1c913d26484
blck-snwmn <whitesnowmancreator@gmail.com>
2023-05-09 23:29:46 +0900

    Extract function

--- main.go
+++ main.go
@@ -12,32 +12,36 @@ import (
 func main() {
 	accountID := os.Getenv("CF_ACCOUNT_ID")
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// This configures the AWS provider to use the Cloudflare R2 endpoint.
-		// See: https://developers.cloudflare.com/r2/examples/terraform/
-		p, err := aws.NewProvider(ctx, "aws.cloudflare_r2", &aws.ProviderArgs{
-			Profile:                   pulumi.String("pulumir2"), // your profile name
-			Region:                    pulumi.String("auto"),
-			SkipCredentialsValidation: pulumi.Bool(true),
-			SkipRegionValidation:      pulumi.Bool(true),
-			SkipRequestingAccountId:   pulumi.Bool(true),
-			SkipMetadataApiCheck:      pulumi.Bool(true),
-			Endpoints: aws.ProviderEndpointArray{
-				aws.ProviderEndpointArgs{
-					S3: pulumi.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID)),
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-		// Note: Use NewBucketV2 instead of NewBucket to create a bucket.
-		// because NewBucket return error:
-		// 	`S3 Bucket acceleration configuration: NotImplemented: GetBucketAccelerateConfiguration not implemented`
-		// See: https://github.com/pulumi/pulumi-aws/pull/1859
-		_, err = s3.NewBucketV2(ctx, "my-bucket", nil, pulumi.Provider(p))
-		if err != nil {
-			return err
-		}
-		return nil
+		return createBucket(ctx, accountID)
 	})
+}
+
+func createBucket(ctx *pulumi.Context, accountID string) error {
+	// This configures the AWS provider to use the Cloudflare R2 endpoint.
+	// See: https://developers.cloudflare.com/r2/examples/terraform/
+	p, err := aws.NewProvider(ctx, "aws.cloudflare_r2", &aws.ProviderArgs{
+		Profile:                   pulumi.String("pulumir2"), // your profile name
+		Region:                    pulumi.String("auto"),
+		SkipCredentialsValidation: pulumi.Bool(true),
+		SkipRegionValidation:      pulumi.Bool(true),
+		SkipRequestingAccountId:   pulumi.Bool(true),
+		SkipMetadataApiCheck:      pulumi.Bool(true),
+		Endpoints: aws.ProviderEndpointArray{
+			aws.ProviderEndpointArgs{
+				S3: pulumi.String(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountID)),
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	// Note: Use NewBucketV2 instead of NewBucket to create a bucket.
+	// because NewBucket return error:
+	// 	`S3 Bucket acceleration configuration: NotImplemented: GetBucketAccelerateConfiguration not implemented`
+	// See: https://github.com/pulumi/pulumi-aws/pull/1859
+	_, err = s3.NewBucketV2(ctx, "my-bucket", nil, pulumi.Provider(p))
+	if err != nil {
+		return err
+	}
+	return nil
 }
